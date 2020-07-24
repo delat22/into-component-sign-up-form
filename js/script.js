b488b6594aada09fa515481685a8fdee7284b74e
delat22 <babalolalateef18@gmai.com>
2020-07-24 23:00:53 +0100

    setting resposiveness

--- js/script.js
+++ js/script.js
@@ -4,20 +4,20 @@ const inputForm = document.getElementsByTagName('input');
 
 
 
-
 submitButton.addEventListener('click', ($event) => {
+    $event.preventDefault();
     for (let i = 0; i < inputForm.length; i++){
         if(inputForm[i].value.length === 0 ){
             inputForm[i].style.border = '1px solid red';
-            inputForm[i].style.backgroundSize = '5%'
-            errorMsg[i].style.display = 'inline'
-            inputForm[2].placeholder='email@example.com'
+            inputForm[i].style.backgroundSize = '5%';
+            errorMsg[i].style.display = 'inline';
+            inputForm[2].placeholder='email@example.com';
         }
 
         else{
             inputForm[i].style.border = '1px solid green';
-            inputForm[i].style.backgroundSize = '0'
-            errorMsg[i].style.display = 'none'
+            inputForm[i].style.backgroundSize = '0';
+            errorMsg[i].style.display = 'none';
         }
     }
 });
